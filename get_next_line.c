15f703e4346d53e756e6ff9a663cd27cfd0aa568
Fauna Polaris <max.maxpinheiro@gmail.com>
2023-06-05 13:39:41 -0300

    starting point

---
+++ get_next_line.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fpolaris <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/24 14:35:52 by fpolaris          #+#    #+#             */
+/*   Updated: 2023/05/29 10:01:08 by fpolaris         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	buffer[BUFFER_SIZE];
+	static char	**memory;
+	int	i;
+
+	i = 0;
+	read(fd, buffer, BUFFER_SIZE);
+
+}
